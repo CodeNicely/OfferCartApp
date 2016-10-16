cc3c3c253b63690ea66654f820b95051df81f5f1
AmanAg513 <amanagrawal513@gmail.com>
2016-10-16 19:06:00 +0530

    login and otp

--- app/src/main/java/com/example/aman/offercart_v1/WelcomeScreen/models/WelcomeScreenProvider.java
+++
@@ -1,7 +0,0 @@
-package com.example.aman.offercart_v1.WelcomeScreen.models;
-
-/**
- * Created by aman on 16/10/16.
- */
-public interface WelcomeScreenProvider {
-}
--- app/src/main/java/com/example/aman/offercart_v1/cityScreen/presenter/CityScreenPresenter.java
+++
@@ -1,7 +0,0 @@
-package com.example.aman.offercart_v1.cityScreen.presenter;
-
-/**
- * Created by aman on 15/10/16.
- */
-public interface CityScreenPresenter {
-}
