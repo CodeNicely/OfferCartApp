e6775691329ccbcecce2551e660803098006df61
iket <iket.ag24@gmail.com>
2016-10-19 17:50:38 +0530

    cities and home drawer

--- app/src/main/java/com/example/aman/offercart_v1/cityScreen/view/CityScreenView.java
+++ app/src/main/java/com/example/aman/offercart_v1/cityScreen/view/CityScreenView.java
@@ -1,7 +1,18 @@
 package com.example.aman.offercart_v1.cityScreen.view;
 
+import com.example.aman.offercart_v1.cityScreen.models.data.CityScreenData;
+
+import java.util.List;
+
 /**
  * Created by aman on 15/10/16.
  */
 public interface CityScreenView {
+
+
+    void showLoading(boolean show);
+    void showMessage(String message);
+    void onCityVerified(List<CityScreenData> cityScreenDataList);
+
 }
+
