3884ad694a536d52318eef84d6f4e8598b80bb78
Meghal Agrawal - Developer <meghal.nit@gmail.com>
2017-04-14 13:50:47 +0530

    Improvements and bug fixed

---
+++ app/src/main/java/com/codenicely/discountstore/project_new/shop_admin/shop_register/api/ShopRegisterApi.java
@@ -0,0 +1,40 @@
+package com.codenicely.discountstore.project_new.shop_admin.shop_register.api;
+
+
+import com.codenicely.discountstore.project_new.helper.Urls;
+import com.codenicely.discountstore.project_new.shop_admin.shop_register.data.ShopPreRegistrationData;
+import com.codenicely.discountstore.project_new.shop_admin.shop_register.data.ShopRegisterData;
+
+import okhttp3.MultipartBody;
+import okhttp3.RequestBody;
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Multipart;
+import retrofit2.http.POST;
+import retrofit2.http.Part;
+import rx.Observable;
+
+/**
+ * Created by meghal on 12/10/16.
+ */
+
+public interface ShopRegisterApi {
+
+    @Multipart
+    @POST(Urls.SUB_URL_CREATE_SHOP)
+    Observable<ShopRegisterData> requestShopRegistration(
+            @Part("name") RequestBody name,
+            @Part("mobile") RequestBody mobile,
+            @Part("password") RequestBody password,
+            @Part("description") RequestBody description,
+            @Part("address") RequestBody address,
+            @Part("category") RequestBody category,
+            @Part("city") RequestBody city,
+            @Part MultipartBody.Part image);
+
+
+    @GET(Urls.SUB_URL_CREATE_SHOP)
+    Call<ShopPreRegistrationData> requestPreRegisterData();
+
+
+}
---
+++ app/src/main/java/com/codenicely/discountstore/project_new/shop_admin/shop_register/providers/MockShopRegisterHelper.java
@@ -0,0 +1,27 @@
+package com.codenicely.discountstore.project_new.shop_admin.shop_register.providers;
+
+import android.net.Uri;
+
+import com.codenicely.discountstore.project_new.shop_admin.shop_register.OnPreRegistrationApiResponse;
+import com.codenicely.discountstore.project_new.shop_admin.shop_register.data.ShopRegisterData;
+
+import java.io.IOException;
+
+import rx.Observable;
+
+/**
+ * Created by meghal on 11/10/16.
+ */
+
+public class MockShopRegisterHelper implements ShopRegisterHelper {
+
+    @Override
+    public Observable<ShopRegisterData> registerShop(String name, String mobile, String password, String description, String address, String category, String city, Uri imageUri) throws IOException {
+        return null;
+    }
+
+    @Override
+    public void requestPreRegistrationDetails(OnPreRegistrationApiResponse onPreRegistrationApiResponse) {
+        // do nothing
+    }
+}
