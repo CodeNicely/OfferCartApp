5bb9b522ad3c860ff49d35e105e19773ab4b06f2
ujjwalagrawal17 <ujjwal.iitism@gmail.com>
2017-06-16 01:50:03 +0530

    Bug fixed my offers

--- app/src/main/java/com/codenicely/brandstore/project/shop_admin/shop_home/ShopHomePage.java
+++ app/src/main/java/com/codenicely/brandstore/project/shop_admin/shop_home/ShopHomePage.java
@@ -18,7 +18,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.MenuItem;
-import android.widget.Button;
 
 import com.codenicely.brandstore.project.R;
 import com.codenicely.brandstore.project.about_us.view.AboutUsFragment;
